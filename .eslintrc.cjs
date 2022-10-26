b7ac9aa6fc2bfe6ab3f51a4b4f21b3e4b5fca48c
casio25 <mishakolomiets355@gmail.com>
2022-10-27 02:08:17 +0300

    check plugin n

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,5 +16,15 @@ module.exports = {
     sourceType: "module",
   },
   plugins: ["sonarjs", "prettier", "import"],
-  rules: {},
+  rules: {
+    "n/exports-style": ["error", "module.exports"],
+    "n/file-extension-in-import": ["error", "always"],
+    "n/prefer-global/buffer": ["error", "always"],
+    "n/prefer-global/console": ["error", "always"],
+    "n/prefer-global/process": ["error", "always"],
+    "n/prefer-global/url-search-params": ["error", "always"],
+    "n/prefer-global/url": ["error", "always"],
+    "n/prefer-promises/dns": "error",
+    "n/prefer-promises/fs": "error",
+  },
 };
